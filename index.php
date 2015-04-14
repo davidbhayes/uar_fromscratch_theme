e96756a86425abb08fed45ac4887d5b5c0b1762f
David Hayes <dhayes@gmail.com>
2015-04-14 16:55:28 -0600

    After #7

--- index.php
+++ index.php
@@ -1,15 +1,23 @@
-<?php 
-wp_head();
-
-if (have_posts()) {
-	while(have_posts()) {
-		the_post();
-		the_title();
-		the_content();
-		the_date();
-	}
-} else {
+<html>
+	<head>
+		<?php wp_head(); ?>
+	</head>
+</html>
+<?php
+if (have_posts()):
+	while(have_posts()):
+		the_post(); ?>
+	<section>
+		<h2><?php the_title(); ?></h2>
+		<?php the_content(); ?>
+		<?php the_date(); ?>
+	</section>
+<?php
+	endwhile;
+else:
 	echo '404! Not found!';
-}
-
-wp_footer();
+endif;
+?>
+<footer>
+	<?php wp_footer(); ?>
+</footer>
