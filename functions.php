93dc637449162bed9183fb88d7d956a69c335fd4
David Hayes <dhayes@gmail.com>
2015-04-29 13:50:16 -0600

    After 10, which changes the way stylesheet included

--- functions.php
+++ functions.php
@@ -2,6 +2,12 @@
 
 add_action( 'wp_enqueue_scripts', 'from_scratch_scripts' );
 function from_scratch_scripts() {
-	wp_enqueue_style( 'main-style', get_stylesheet_uri() );
+	/* If using a child theme, auto-load the parent theme style. */
+    if ( is_child_theme() ) {
+        wp_enqueue_style( 'parent-style', trailingslashit( get_template_directory_uri() ) . 'style.css' );
+    }
+
+    /* Always load active theme's style.css. */
+    wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
 
