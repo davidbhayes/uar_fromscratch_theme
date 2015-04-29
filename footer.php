964659ba63128e00cd547e4eff25eb997e39c0d5
David Hayes <dhayes@gmail.com>
2015-04-29 15:29:21 -0600

    After 14, have sidebar, header, footer each in own file.

---
+++ footer.php
@@ -0,0 +1,10 @@
+			</div>
+			<?php get_sidebar(); ?>
+		</div>
+		<footer class="site-footer">
+			<p>Made pretty by the FromScratch Theme</p>
+			<p>Proudly powered by <a href="http://www.wordpress.org/">WordPress</a></p>
+		</footer>
+		<?php wp_footer(); ?>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,55 +1,33 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<?php wp_head(); ?>
-	</head>
-	<body <?php body_class(); ?>>
-		<div class="site container">
-			<div id="content" class="site-content">
-				<header class="site-header">
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php from_scratch_primary_menu(); ?>
-				</header>
-				<main class="site-main">
-					<?php
-					if (have_posts()):
-						while(have_posts()):
-							the_post(); ?>
-						<section <?php post_class(); ?>>
-							<header>
-								<a href="<?php the_permalink(); ?>">
-									<h2 class="post-title"><?php the_title(); ?></h2>
-								</a>
-								<p class="byline">
-									by <?php the_author(); ?> on <?php the_date(); ?>
-								</p>
-							</header>
-							<?php the_content(); ?>
-							<footer>
-								<a href="<?php the_permalink(); ?>">&infin;</a>
-							</footer>
-						</section>
-					<?php
-						endwhile;
-					else: ?>
-						<section>
-							<header>
-								<h1>404! Not found!</h1>
-							</header>
-						</section>
-					<?php 
-					endif;
-					?>
-				</main>
-			</div>
-			<aside class="sidebar">
-				<?php dynamic_sidebar( 'sidebar' ); ?>
-			</aside>
-		</div>
-		<footer class="site-footer">
-			<p>Made pretty by the FromScratch Theme</p>
-			<p>Proudly powered by <a href="http://www.wordpress.org/">WordPress</a></p>
-		</footer>
-		<?php wp_footer(); ?>
-	</body>
-</html>
+<?php get_header(); ?>
+<main class="site-main">
+	<?php
+	if (have_posts()):
+		while(have_posts()):
+			the_post(); ?>
+		<section <?php post_class(); ?>>
+			<header>
+				<a href="<?php the_permalink(); ?>">
+					<h2 class="post-title"><?php the_title(); ?></h2>
+				</a>
+				<p class="byline">
+					by <?php the_author(); ?> on <?php the_date(); ?>
+				</p>
+			</header>
+			<?php the_content(); ?>
+			<footer>
+				<a href="<?php the_permalink(); ?>">&infin;</a>
+			</footer>
+		</section>
+	<?php
+		endwhile;
+	else: ?>
+		<section>
+			<header>
+				<h1>404! Not found!</h1>
+			</header>
+		</section>
+	<?php 
+	endif;
+	?>
+</main>
+<?php get_footer(); ?>
