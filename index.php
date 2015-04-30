dd764fc96994c1eb6e51894bcb599175a28459b3
David Hayes <dhayes@gmail.com>
2015-04-29 19:07:06 -0600

    After 21, get_template_part post rendering

--- index.php
+++ index.php
@@ -3,25 +3,9 @@
 	<?php
 	if (have_posts()):
 		while(have_posts()):
-			the_post(); ?>
-		<section <?php post_class(); ?>>
-			<header>
-				<div class="post-image">
-					<?php the_post_thumbnail( 'better-thumb' ); ?>
-				</div>
-				<a href="<?php the_permalink(); ?>">
-					<h2 class="post-title"><?php the_title(); ?></h2>
-				</a>
-				<p class="byline">
-					by <?php the_author(); ?> on <?php the_date(); ?>
-				</p>
-			</header>
-			<?php the_content(); ?>
-			<footer>
-				<a href="<?php the_permalink(); ?>">&infin;</a>
-			</footer>
-		</section>
-	<?php endwhile ?>
+			the_post(); 
+				get_template_part( 'content', 'entity' );
+		endwhile ?>
 		<div class="posts-nav">
 			<?php next_posts_link('older'); ?>
 			&nbsp;
