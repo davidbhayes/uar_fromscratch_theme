fe62980d1a56da10955ff2e2a85150a07c1cf160
David Hayes <dhayes@gmail.com>
2015-05-01 14:42:10 -0600

    After 25, basic page template

---
+++ archive-template.php
@@ -0,0 +1,33 @@
+<?php 
+/*
+Template Name: Archive Page
+*/
+?>
+<?php get_header(); ?>
+<main class="site-main">
+	<?php
+	if ( have_posts() ):
+		while ( have_posts() ):
+			the_post(); 
+				if ( is_single() ) {
+					get_template_part( 'content', 'single' );
+				} else {
+					get_template_part( 'content', 'loop' );
+				}
+		endwhile; ?>
+		<div class="posts-nav">
+			<?php next_posts_link( 'older' ); ?>
+			&nbsp;
+			<?php previous_posts_link( 'newer' ); ?>
+		</div>
+	<?php else: ?>
+		<section>
+			<header>
+				<h1>404! Not found!</h1>
+			</header>
+		</section>
+	<?php 
+	endif;
+	?>
+</main>
+<?php get_footer(); ?>
